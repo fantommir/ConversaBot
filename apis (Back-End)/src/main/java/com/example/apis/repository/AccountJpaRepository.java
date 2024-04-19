453bfb5462ec86551e0097acba87671bf2fefe9b
NIHALSAYYAD15 <90887293+NIHALSAYYAD15@users.noreply.github.com>
2024-04-19 16:38:17 +0530

    Update AccountJpaRepository.java

--- apis (Back-End)/src/main/java/com/example/apis/repository/AccountJpaRepository.java
+++ apis (Back-End)/src/main/java/com/example/apis/repository/AccountJpaRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.apis.model.Account;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
 
 @Repository
 public interface AccountJpaRepository extends JpaRepository<Account, Integer> {
+      Optional<Account> findByMailId(String email);
+
 
 }
